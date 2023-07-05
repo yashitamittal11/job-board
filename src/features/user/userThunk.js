e1ad76ea35d1d3202b8313e0a89e19832a633653
yashita.mittal <yashita.mittal@a8c.com>
2023-07-05 17:20:49 +0530

    Add the job page functionality

--- src/features/user/userThunk.js
+++ src/features/user/userThunk.js
@@ -21,9 +21,18 @@ export const loginUserThunk = async ( url, user, thunkAPI ) => {
 
 export const updateUserThunk = async ( url, user, thunkAPI ) => {
   try {
-    const resp = await customFetch.patch( url, user );
+    const resp = await customFetch.patch( url, user, {
+      headers: {
+        authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+      },
+    });
     return resp.data;
   } catch ( error ) {
+    // console.log(error.response);
+    if ( error.response.status === 401 ) {
+      thunkAPI.dispatch( logoutUser() );
+      return thunkAPI.rejectWithValue( 'Unauthorized! Logging Out...' );
+    }
     return thunkAPI.rejectWithValue( error.response.data.msg );
   }
 };
